e5d9b03f32a73fcf1a9c0a9e4f6441e2b6a497f4
pedrofbaltar <pfbaltariead@gmai.com>
2021-05-01 01:22:12 -0300

    Generating and executing the project build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,12 @@ export default function Home(props) {
 
 export async function getStaticProps() {
   const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json()
+  const data = await response.json();
 
   return {
     props: {
       episodes: data,
     },
-    reavalidate: 60 * 60 * 8,
+    revalidate: 60 * 60 * 8,
   }
 }
