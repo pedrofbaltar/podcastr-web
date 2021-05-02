b301a26a77e195d7334ca0ee9b7fad41bd039c24
pedrofbaltar <pfbaltariead@gmai.com>
2021-05-02 13:30:31 -0300

    Creating Player Context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,31 @@ import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
 
-      <Player />
-    </div>
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
+  return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,12 +1,15 @@
 import format from 'date-fns/format';
-import ptBR from 'date-fns/locale/pt-BR';
 import parseISO from 'date-fns/parseISO';
-import { GetStaticPaths, GetStaticProps } from 'next';
+import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
+
 import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
 import styles from './episode.module.scss';
 
 type Episode = {
@@ -26,8 +29,6 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
-  const router = useRouter()
-
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -66,8 +67,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  });
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  });
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
